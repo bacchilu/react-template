dc91db770c31dd1c624fb44c7267b91b05430427
Luca Bacchi <bacchilu@gmail.com>
2022-02-08 16:05:28 +0100

    Bug fix

--- reactjs-project/js/src/index.js
+++ reactjs-project/js/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = function (props) {
-    return <p>Hello, world!</p>;
+    return (
+        <>
+            <p>Nel mezzo del cammin di nostra vita</p>
+            <p>mi ritrovai per una selva oscura,</p>
+            <p>ché la diritta via era smarrita.</p>
+        </>
+    );
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
