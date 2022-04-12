c94109fa2cfc43fefefa0617ee635ebaec5ef056
Luca Bacchi <bacchilu@gmail.com>
2022-04-12 15:51:34 +0200

    React 18 for Parcel

--- parcel/src/js/index.js
+++ parcel/src/js/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 
 const App = function () {
     return (
@@ -13,4 +13,4 @@ const App = function () {
     );
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+createRoot(document.getElementById('app')).render(<App />);
