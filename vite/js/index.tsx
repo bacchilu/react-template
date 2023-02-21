ef3edee4908d8464f0d851f3afcdd78d4880953b
Luca Bacchi <bacchilu@gmail.com>
2023-02-21 16:46:19 +0100

    Vite bundler

---
+++ vite/js/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+const App = function () {
+    return (
+        <div className="alert alert-secondary" role="alert">
+            <blockquote className="blockquote">
+                <p>Nel mezzo del cammin di nostra vita</p>
+                <p>mi ritrovai per una selva oscura,</p>
+                <p>ché la diritta via era smarrita.</p>
+            </blockquote>
+        </div>
+    );
+};
+
+createRoot(document.getElementById('app')!).render(<App />);
