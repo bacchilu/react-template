67fc454bb38f30103a053de11e413b7ea7f7aa95
Luca Bacchi <bacchilu@gmail.com>
2023-02-23 14:51:46 +0100

    No prettier

--- vite/js/index.tsx
+++ vite/js/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
 
 const App = function () {
-    return (
-        <div className="alert alert-secondary" role="alert">
-            <blockquote className="blockquote">
-                <p>Nel mezzo del cammin di nostra vita</p>
-                <p>mi ritrovai per una selva oscura,</p>
-                <p>ché la diritta via era smarrita.</p>
-            </blockquote>
-        </div>
-    );
+  return (
+    <div className="alert alert-secondary" role="alert">
+      <blockquote className="blockquote">
+        <p>Nel mezzo del cammin di nostra vita</p>
+        <p>mi ritrovai per una selva oscura,</p>
+        <p>ché la diritta via era smarrita.</p>
+      </blockquote>
+    </div>
+  );
 };
 
-createRoot(document.getElementById('app')!).render(<App />);
+createRoot(document.getElementById("app")!).render(<App />);
